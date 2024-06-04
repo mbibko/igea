e0f92c98672506ac4cf07f94f0cd16c680cb58fe
mbibko <mbibko@gmail.com>
2024-06-04 14:57:44 +0300

    added responsive header, hero section, footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
+  content: ["*.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
     container: {
       center: true,
+      padding: '2rem',
       screens: {
-        sm: "600px", // Adjust as needed for small screens
-        md: "728px", // Adjust as needed for medium screens
-        lg: "984px", // Adjust as needed for large screens
-        xl: "1240px", // Adjust as needed for extra large screens
-        "2xl": "1680px", // Adjust as needed for extra extra large screens
+        "2xl": "1616px", // Adjust as needed for extra extra large screens
       },
     },
     extend: {},
