bf7f954980a25fe9faae1ca73bb078d2c8ed0718
mbibko <mbibko@gmail.com>
2024-06-06 12:38:42 +0300

    updated config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import nunjucks from "vite-plugin-nunjucks";
+const { resolve } = require("path");
 
 export default {
   base: "/igea/",
   plugins: [nunjucks()],
+  build: {
+    rollupOptions: {
+      input: [
+        resolve(__dirname, "index.html"),
+        resolve(__dirname, "catalog.html"),
+      ],
+    },
+  },
 };
